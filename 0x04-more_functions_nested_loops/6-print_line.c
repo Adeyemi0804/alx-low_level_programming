facae00a51906275afcbc5396424041d26425971
Adeyemi0804 <abdulazeezadeyinka2004@gmail.com>
2023-08-17 04:34:07 -0700

    kk

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,16 +9,13 @@ void print_line(int n)
 {
 	if (n <= 0)
 	{
-		_putchar('\n');
+		_putchar ('\n');
+		return;
 	}
-	else
+	while (n > 0)
 	{
-		int i;
-
-		for (i = 1; i <= n; i++)
-		{
-			_putchar('_');
-		}
-		_putchar('\n');
+		_putchar('_');
+		n--;
 	}
+	_putchar ('\n');
 }
