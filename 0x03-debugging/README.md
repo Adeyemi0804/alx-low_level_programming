45c1f991df2bbe19c1e613b5790e3b5f6d8575c0
Adeyemi0804 <abdulazeezadeyinka2004@gmail.com>
2023-08-17 06:32:37 -0700

    kk

---
+++ 0x03-debugging/README.md
@@ -0,0 +1,1 @@
+uerjew`:x
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,11 @@
+#ifndef
+#define
+
+int main(void);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+int _putchar(char);
+void positive_or_negative(int i);
+
+#endif	
