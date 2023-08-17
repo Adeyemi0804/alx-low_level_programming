093ec615f587a673d34ae2924995380993fccaad
Adeyemi0804 <abdulazeezadeyinka2004@gmail.com>
2023-08-17 06:03:53 -0700

    kk

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,25 +1,25 @@
 #include "main.h"
 
 /**
- * print_diagonal - Prints a diagonal line of backslashes with indentation
- * @n: input variables
+ * print_diagonal - Prints a diagonal line of backlashes with indentation
+ * @n; input variables
  *
  * Return: void
  */
 
 void print_diagonal(int n)
 {
-        int i, j;
+	in i, j;
 
-        if (n <= 0)
-                _putchar('\n');
-        for (i = 0; i < n; i++)
-        {
-                for (j = 0; j < i; j++)
-                {
-                        _putchar(' ');
-                }
-                _putchar('\\');
-                _putchar('\n');
-        }
+	if (n <= 0)
+		_putchar('\n');
+	for (i = 0; i < n; i++)
+	{
+		for (j = 0; j < 1; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,22 +1,25 @@
 #include "main.h"
 
 /**
- * print_square - Prints a square made of '#' characters
- * @size: The size of the square
+ * print_square - prints a square, followed by a new line;
+ * @size: size of the square
  */
-
 void print_square(int size)
 {
-        int a, b;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
 
-        if (size <= 0)
-                _putchar('\n');
-        for (a = 0; a < size; a++)
-        {
-                for (b = 0; b < size; b++)
-                {
-                        _putchar('#');
-                }
-                _putchar('\n');
-        }
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
