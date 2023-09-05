f6406305e431a7bcab0fb7e188a681cd20d7b577
Adeyemi0804 <abdulazeezadeyinka2004@gmail.com>
2023-09-04 23:06:41 -0700

    deed

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * Create_array - an array of size with char c
+ * @size: Unsigned int type
+ * @c: char type
+ * Return: Return pointer to array created
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	a = malloc((size) * sizeof(char));
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		a[i] =c;
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
+}
---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/*
+ * _strlen - function to return length of a function
+ *
+ * @s: char type
+ *
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int a;
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+	}
+	return (a);
+}
+
+
+/**
+ * *_strdup - the function to return the pointer to the string
+ *
+ *  @str: The pointer to string array input
+ *
+ *  Return: pointer to string created
+ */
+
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int size;
+	int x;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	size = _strlen(str) + 1;
+	ptr = malloc(size * sizeof(char));
+
+	x = 0;
+	while (x < size)
+	{
+		if (ptr == NULL)
+		{
+			return (NULL);
+		}
+		ptr[x] = str[x];
+		x++;
+	}
+	return (ptr);
+}
