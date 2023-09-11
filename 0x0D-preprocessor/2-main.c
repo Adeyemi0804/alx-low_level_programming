b05a221a1bc60874f4e6b4a75f49bd4c7d47475f
Adeyemi0804 <abdulazeezadeyinka0804@gmail.com>
2023-09-11 14:06:41 +0100

    tyy

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,11 @@
 
 /**
  *
- * main - preints the name of the file
+ * main - prints the name of the file
  * 
  *  Return: Always 0 (Success)
- * /
+ */
+
 int main(void)
 {
 		printf("%s\n", __FILE__);
